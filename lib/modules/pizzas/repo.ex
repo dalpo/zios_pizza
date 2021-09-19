f3c5fdd88e967f36fc34691be8da7d0d8d701e18
ema <emanuele@codiceplastico.com>
2021-09-19 16:46:56 +0200

    use utils functions

--- lib/modules/pizzas/repo.ex
+++ lib/modules/pizzas/repo.ex
@@ -1,24 +1,9 @@
 defmodule ZiosPizza.Pizzas.Repo do
   alias ZiosPizza.Pizzas.Schema
+  alias ZiosPizza.Utils.Functions
 
   def get_all do
     ZiosPizza.Repo.all(Schema)
-    |> Enum.map(&serialize_to_map/1)
-  end
-
-  defp serialize_to_map(struct) do
-    map =
-      struct
-      |> Map.delete(:__meta__)
-      |> Map.from_struct()
-
-    Enum.reduce(Map.keys(map), map, fn m, acc ->
-      with true <- is_map(acc[m]),
-           true <- Map.has_key?(acc[m], :__meta__) do
-        Map.put(acc, m, serialize_to_map(acc[m]))
-      else
-        _ -> acc
-      end
-    end)
+    |> Enum.map(&Functions.serialize_to_map/1)
   end
 end
